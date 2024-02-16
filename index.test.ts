4439b7554cbba92779e3610114a25430b63d34ce
yamin.gherbi <y.gherbi.ext@axione.fr>
2024-02-16 15:31:41 +0100

    refaco description of tests

--- index.test.ts
+++ index.test.ts
@@ -1,14 +1,20 @@
 import { expect, describe, test } from 'vitest'
 import {calculateShoppingCart} from "./index";
-import {BOOK_1, BOOK_2, EmptyShoppingCard} from "./data";
+import {BOOK_1, BOOK_2, BOOK_3, BOOK_4, EmptyShoppingCard} from "./data";
 
 describe('calculate shopping cart', () => {
-    test('it should receive 0 when you cart is empty', () => {
+    test('it should return a total price of 0 for an empty cart', () => {
         expect(calculateShoppingCart(EmptyShoppingCard)).toEqual(0)
     });
 
-    test('it should receive 8 when you cart contain 1 book', () => {
+    test('it should return a total price of 8 EUR for a cart with a single book', () => {
         expect(calculateShoppingCart([BOOK_1])).toEqual(8)
         expect(calculateShoppingCart([BOOK_2])).toEqual(8)
+    })
+
+    test('it should not apply discounts for multiple copies of the same book', () => {
+        expect(calculateShoppingCart([BOOK_1, BOOK_1])).toEqual(16)
+        expect(calculateShoppingCart([BOOK_2, BOOK_2])).toEqual(16)
+        expect(calculateShoppingCart([BOOK_4, BOOK_4, BOOK_4, BOOK_4])).toEqual(32)
     })
 })
