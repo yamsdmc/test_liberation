a60650cedabb23d9406421cb4f4c39fb1596f4a8
yamin.gherbi <y.gherbi.ext@axione.fr>
2024-02-16 16:02:21 +0100

    refacto

--- index.ts
+++ index.ts
@@ -1,21 +1,30 @@
 export type Book = string
 
 const standardPrice = 8;
+
+const FIVE_PERCENT = 0.95
+const TEN_PERCENT = 0.90
+const TWENTY_PERCENT = 0.80
+const TWENTYFIVE_PERCENT = 0.75
+
+const discounts: Record<number, number> = {
+    2: FIVE_PERCENT,
+    3: TEN_PERCENT,
+    4: TWENTY_PERCENT,
+    5: TWENTYFIVE_PERCENT
+}
+
 export function calculateShoppingCart(books: Book[]): number {
     if(!books.length) {
         return 0
     }
 
     const uniqueBooks = new Set(books);
-
-    if(uniqueBooks.size === 2) {
-        return uniqueBooks.size * standardPrice * 0.95
-    } else if(uniqueBooks.size === 3) {
-        return uniqueBooks.size * standardPrice * 0.90
-    } else if(uniqueBooks.size === 4) {
-        return uniqueBooks.size * standardPrice * 0.80
-    } else if(uniqueBooks.size === 5) {
-        return uniqueBooks.size * standardPrice * 0.75
-    }
-    return books.length * standardPrice
+    const discount = getDiscount(uniqueBooks.size)
+    return books.length * standardPrice * discount
 }
+
+function getDiscount(uniqueBookNumber: number) {
+    return discounts[uniqueBookNumber] || 1
+}
+
